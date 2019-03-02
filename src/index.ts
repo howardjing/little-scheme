4416b79a51a8e85a890512bc54a2f7245b5b907f
Howard Jing <howard.jing@gmail.com>
2019-03-02 15:01:44 -0500

    handle atoms

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
-const expressions = [
-  1,
-  [1],
-  [1,2],
-  ['+', 1, 2],
-]
 
 type Atom = number | string;
-type Expression = Atom | Atom[];
+
+type Expression = Atom | ExpressionArray;
+// https://github.com/Microsoft/TypeScript/issues/3988 -- workaround for type Expression = Atom | Expression[]
+interface ExpressionArray extends Array<Expression> {}
+
 
 const apply = (expression: Expression) => {
   if (isAtom(expression)) { return expression }
-  console.error("UNSUPPORTED EXPRESSION", expression);
+
+  throw new Error(`Unsupported expression ${JSON.stringify(expression)}`)
 }
 
 const isAtom = (expression: Expression) => !Array.isArray(expression);
 
+const expressions: Expression[] = [
+  1,
+  ["quote", 1],
+  ["quote", [1,2]],
+  ['+', 1, 2],
+]
+
 expressions.forEach(expression => {
-  console.log(expression, apply(expression))
+  try {
+    console.log("EVALUATING", expression);
+    console.log(apply(expression))
+  } catch (e) {
+    console.error(e)
+  }
 })
